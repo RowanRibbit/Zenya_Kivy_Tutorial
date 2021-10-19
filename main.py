d8d29a284304216ca1aba48c1ec3ef83309f71fe
Rowan Thompson <92335126+RowanRibbit@users.noreply.github.com>
2021-10-19 17:12:22 +0000

    keeps crashing even after referencing code to the source, don't know why

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ from kivy.core.window import Window
 from kivy.uix.popup import Popup
 from kivy.uix.image import Image
 from kivy.uix.modalview import ModalView
+from kivy.clock import Clock
 
 from random import randint
+
 
 # properties allow us to attach widgets to state maintained in our python files - promises a widget that it will have a variable which will be updated in the Python file, which is updated automatically in the Kivy file
 # i.e., Name property > Updated to new name. With Kivy, tell the label widget is expecting a string. If the string changes, it updates in the widget itelf.
@@ -22,8 +24,12 @@ Window.size = (300,400)
 # Want to display this when the timer is up or the player finishes the game
 # On exit reset the game
 class EndGameScreen(ModalView):
-  # dismiss function
-  pass
+  final_score = NumericProperty(0)
+  high_score = NumericProperty(0)
+
+  def update_scores(self, final_score, high_score):
+    self.final_score = final_score
+    self.high_score = high_score
 
 
 # custom widget for the images
@@ -39,25 +45,35 @@ class ProgressBar(Widget):
 
 # gamescreen class, subclass of widget
 class GameScreen(Widget):
-
-
-
   # get the item names
   item_names = [
     'bote', 'diamante', 'espada', 'hacha', 'ladrillos', 'manzana', 'pala', 'pasto', 'roca', 'tierra'
   ]
+  seconds = 0
+  is_game_over = False
+  score = 0
+  high_score = 0
   # numeric property for correct answer index
   correct_answer_index = NumericProperty(0)
   display_image_names = ListProperty(['bote', 'diamante', 'espada', 'hacha'])
   correct_answer_text = StringProperty('bote')
+  bar_width = NumericProperty(0)
   
   def __init__(self, **kwargs):
     super(GameScreen, self).__init__(**kwargs)
     self.load_new_question()
 
+  def make_selection(self, index):
+    # get the button index
+    if self.check_if_answer_correct(index):
+      self.show_answer_popup(True)
+    else:
+      self.show_answer_popup(False)
+
   # check if a selection is correct based on the index
   def check_if_answer_correct(self, index):
     if index == self.correct_answer_index:
+      self.score += 1
       return True
     return False
 
@@ -66,7 +82,6 @@ class GameScreen(Widget):
     # copy the contents of the list with copy()
     temp_items = self.item_names.copy()
     items = []
-
     for _ in range(4):
       rand_index = randint(0,len(temp_items)-1)
       items.append(temp_items[rand_index])
@@ -81,17 +96,10 @@ class GameScreen(Widget):
     self.correct_answer_index = randint(0,3)
     self.correct_answer_text = self.display_image_names[self.correct_answer_index]
     
-  def make_selection(self, index):
-    # get the button index
-    if self.check_if_answer_correct(index):
-      self.show_answer_popup(True)
-    else:
-      self.show_answer_popup(False)
-  
   def show_answer_popup(self, is_correct):
     content = Image(source='Images/'+self.correct_answer_text+'.png')
     # call function to change the score based on correct
-    popup = Popup(
+    self.answer_popup = Popup(
       title = 'Correct' if is_correct else 'Incorrect',
       size_hint=(.5,.4),
       content=content,
@@ -101,21 +109,46 @@ class GameScreen(Widget):
       background='Images/white.png',
       # when dismiss popup call a function    
     )
-    popup.bind(on_dismiss=self.load_new_question)
+    self.answer_popup.bind(on_dismiss=self.load_new_question)
     # creates a popup but doesnt attach it to anything
-    popup.open()
+    self.answer_popup.open()
 
-  def show_endgame_popup(self):
+  def show_end_game_popup(self):
     end_game_screen = EndGameScreen(
       size_hint=(.75, .6), 
       auto_dismiss=False)
+    end_game_screen.update_scores (self.score, self.high_score)
     end_game_screen.open()
+  
+  def increase_bar_width(self, *args):
+    if self.is_game_over:
+      return
+    width_increase = ((800*.8)-10/60)
+    self.bar_width += width_increase
+    self.seconds += 1
+    if self.seconds >= 60:
+      self.execute_end_game()
+
+  def execute_end_game(self):
+    self.is_game_over = True
+    if self.score > self.high_score:
+      self.high_score = self.score
+    self.show_end_game_popup()
+
+  def reset(self, *args):
+    self.bar_width = 0
+    self.is_game_over = False
+    self.seconds = 0
+    self.score = 0
+    self.answer_popup.dismiss
+    self.load_new_question()
 
 # create a kivy file which is the same as the app name, but all lower case
 class LanguageLearnerApp(App):
 
     def build(self):
       game_screen = GameScreen()
+      Clock.schedule_interval(game_screen.increase_bar_width, 60.0/60.0)
       return game_screen
         
     
